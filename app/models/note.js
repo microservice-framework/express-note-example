dd99334224cfebd4a6a657b401466b662311e491
Gor Martsen <Gor.martsen@gmail.com>
2018-03-02 08:48:19 -0500

    code style - tabs

--- app/models/note.js
+++ app/models/note.js
@@ -2,8 +2,8 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var NoteSchema   = new Schema({
-    title: String,
-    body: String
+  title: String,
+  body: String
 });
 
 module.exports = mongoose.model('note', NoteSchema);
